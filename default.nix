1ceee765f8bdd7782fae2781880a5d899614c3e2
Break Yang <breakds@gmail.com>
2023-09-10 17:38:42 -0700

    Add flake

---
+++ default.nix
@@ -0,0 +1,38 @@
+{ lib
+, buildPythonPackage
+, backoff
+, joblib
+, openai
+, pandas
+, spacy
+, regex
+, ujson
+, tqdm
+, datasets
+}:
+
+buildPythonPackage rec {
+  pname = "dspy";
+  version = "2023.09.10";
+
+  src = ./.;
+
+  propagatedBuildInputs = [
+    backoff
+    joblib
+    openai
+    pandas
+    spacy
+    regex
+    ujson
+    tqdm
+    datasets
+  ];
+
+  meta = with lib; {
+    homepage = "https://github.com/stanfordnlp/dspy";
+    description = "DSPy: The framework for programming with foundation models";
+    license = licenses.mit;
+    maintainers = with maintainers; [ breakds ];
+  };
+}
